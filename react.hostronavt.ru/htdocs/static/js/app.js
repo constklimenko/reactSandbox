f19395b6e537bb4624894f392b7abd1e8e774e7d
narcom <narcomacer@gmail.com>
2019-11-23 10:18:45 +0500

    добавлена интерактивность в учебный класс

--- react.hostronavt.ru/htdocs/static/js/app.js
+++ react.hostronavt.ru/htdocs/static/js/app.js
@@ -1,24 +1,31 @@
 
 var my_news = [{
   author: 'Саша Печкин',
-  text: 'В четверг, четвертого числа...'
+  text: 'В четчерг, четвертого числа...',
+  bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили xёрными чернилами чертёж.'
 },
-{
-  author: 'Просто Вася',
-  text: 'Считаю, что $ должен стоить 35 рублей!'
-},
-{
-  author: 'Гость',
-  text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
 },
   {
     author: 'Дюбель',
-    text: 'Бум бум бум'
+    text: 'Бум бум бум',
+    bigText: 'мазафака бум'
+
+  
   },
 
   {
     author: 'Барракуда',
-    text: 'Съем всех человеков'
+    text: 'Съем всех человеков',
+    bigText: 'оставлю только самых тощих'
   },
 
 ];
@@ -36,12 +43,39 @@ class Comments extends React.Component {
 }
 
 class Article extends React.Component {
+
+  static propTypes(){
+    data: React.propTypes.shape({
+      author: React.propTypes.string.isRequired,
+      text: React.propTypes.string.isRequired,
+      bigText: React.propTypes.string.isRequired,}
+      
+    )
+
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  
   render() {
     var item = this.props.data;
+    var visible = this.state.visible;
+
 
     return (<div className="article">
       <p className="news__author">{item.author}:</p>
       <p className="news__text">{item.text}</p>
+      <a href="#" className={'news__readmore ' + (visible ? 'none' : '')}
+      onClick={() => this.setState({
+        visible: true
+      })}
+      >Подробнее</a>
+      <p className={'news__big-text ' + (visible ? '' : 'none')}  >{item.bigText}</p>
       </div>
 
     );
